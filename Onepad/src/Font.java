22af462a0ec1cb134cc6088bf2abb1b5a4980677
honk2333 <34983184+honk2333@users.noreply.github.com>
2019-07-26 20:33:33 +0800

    Delete Font.java

--- Onepad/src/Font.java
+++
@@ -1,9 +0,0 @@
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.GraphicsEnvironment;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
-
-import javax.swing.JCheckBox;
-import javax.swing.JComboBox;
-
